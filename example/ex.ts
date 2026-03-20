af38c6eaf2cd09aeb4903f306cd8a6c45534f2f4
Jakub Bystroński <jbstr85@gmail.com>
2026-03-21 00:08:26 +0100

    rest

--- example/ex.ts
+++ example/ex.ts
@@ -9,17 +9,6 @@ type BaseCtx = {
   indexMap: Map<string, string>;
   defString: string;
 };
-// const wf = createWorkflow(registry, context)("processItems");
-//
-// wf.seq("start", "fetchItems")
-//   .forEach(
-//     "processEachItem",
-//     ctx => ctx.results.start,       // items array from previous step
-//     "processItem",                 // action to run for each item
-//     ({ item }) => [item]           // resolve args for action
-//   )
-//   .join("mergeResults", "combine", ctx => [ctx.results.processEachItem])
-//   .output(ctx => ctx.results);
 
 export const regOne = {
   uppercase: async ({ text }: { text: string }) => text.toUpperCase(),
@@ -35,16 +24,20 @@ export const regOne = {
 const modWithLoop = createModule({
   registry: regOne,
   context: {},
-  define: ({ wf, deps, loop, args }) => {
+  define: ({ wf, deps }) => {
+    const uppercase = wf<{ v: string }>("uppercase")
+      .seq("a", "uppercase", (ctx) => ctx.obj({ text: ctx.input.v }))
+      .output((ctx) => ctx.results.a);
     const loopWorkflow = wf<{ items: string[] }>("loop")
-      .seq("results", "lowercase", (ctx) => loop(ctx.input.items.map(args)), {
-        loop: true,
-      })
+      .seq("results", "lowercase", (ctx) =>
+        ctx.loop(ctx.input.items.map((item) => ctx.args(item))),
+      )
 
       .output((ctx) => ctx.results.results);
 
     return {
       loopWorkflow,
+      uppercase,
     };
   },
 });
@@ -55,24 +48,27 @@ const loopRes = await executeWorkflow(modWithLoop.loopWorkflow, regOne, {
 
 console.log("LOOP", loopRes);
 
-// const upTest = await executeWorkflow(modWithLoop.uppercase, regOne, {
-//   v: "dog",
-// });
-//
-// console.log("loopREs", upTest);
+const upTest = await executeWorkflow(modWithLoop.uppercase, regOne, {
+  v: "dog",
+});
+
+console.log("loopREs", upTest);
 
 export const modA = createModule({
   registry: regOne,
   context: { suffix: "!" }, // example of context
-  define: ({ wf, deps, obj }) => {
+  define: ({ wf, deps }) => {
     const jl = wf("jl").seq("#l", "justLog").build();
     const a1 = wf<{ value: string; another: number }>("a1")
-      .seq("upper", "uppercase", (ctx) => obj({ text: ctx.input.value }))
+      .seq("upper", "uppercase", (ctx) => ctx.obj({ text: ctx.input.value }))
 
-      .seq("addSuffix", "uppercase", (ctx) => obj({ text: ctx.input.value }))
+      .seq("addSuffix", "uppercase", (ctx) => ctx.obj({ text: "22" }))
 
       .output((ctx) => ({ result: ctx.results.addSuffix }));
 
+    const a2 = wf<{ v: string }>("a2")
+      .subflow("asub", a1, (ctx) => ({ value: "foo", another: 22 }))
+      .output((ctx) => ({ res: ctx.results.asub.result }));
     return { jl, a1 };
   },
 });
@@ -82,13 +78,11 @@ export const modB = createModule({
   registry: regOne,
   context: { multiplier: 2 }, // B-specific context
   use: [modA] as const, // 👈 reuse A flows
-  define: ({ wf, deps, args }) => ({
+  define: ({ wf, deps }) => ({
     b1: wf<{ value: string }>("b1")
-      .subflow("aFlowPre", deps.jl)
-      .subflow("aFlow", deps.a1, () => ({ value: "a", another: 22 }))
-      .seq("doubleLength", "double", (ctx) =>
-        args(ctx.results.aFlow.result.length * ctx.context.multiplier),
-      )
+      .subflow("aFlowPre", deps.jl, (ctx) => ({}))
+      .subflow("aFlow", deps.a1, () => ({ value: "a", another: 2 }))
+      .seq("doubleLength", "double", (ctx) => ctx.args(300))
       .output((ctx) => ({ result: ctx.results.aFlow.result })),
   }),
 });
@@ -98,12 +92,14 @@ export const modC = createModule({
   registry: regOne,
   context: { prefix: ">>" }, // C-specific context
   use: [modB],
-  define: ({ wf, deps, args }) => ({
+  define: ({ wf, deps }) => ({
     lower: wf<{ v: string }>("lower")
-      .seq("lw", "lowercase", (ctx) => args(ctx.input.v))
-      .parallel((b) => b.seq("p1", "lowercase", (ctx) => args(ctx.results.lw)))
+      .seq("lw", "lowercase", (ctx) => ctx.args(ctx.input.v))
+      .parallel((b) =>
+        b.seq("p1", "lowercase", (ctx) => ctx.args(ctx.results.lw)),
+      )
       .when((ctx) => ctx.results.lw === "ABC")
-      .seq("lw2", "lowercase", (ctx) => args(ctx.results.lw))
+      .seq("lw2", "lowercase", (ctx) => ctx.args(ctx.results.lw))
       .output((ctx) => ctx.results.lw),
     c1: wf<{ value: string }>("c1")
       .subflow("bFlow", deps.b1, (ctx) => ({ value: ctx.input.value }))
