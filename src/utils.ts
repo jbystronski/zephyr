64edfe07b9cb3b031fd7025dc6069f1a645edae6
Jakub Bystroński <jbstr85@gmail.com>
2026-03-27 23:56:56 +0100

    soft delete createRuntime standalone util, redundant and weak

--- src/utils.ts
+++ src/utils.ts
@@ -23,21 +23,21 @@ export function fixedAction<F extends AnyFn>(fn: F) {
     return fn as (...args: Parameters<F>) => ReturnType<F>;
   };
 }
-export function createRuntime<Reg extends ActionRegistry, Context>(
-  registry: Reg,
-  context: Context,
-) {
-  return {
-    run: async <W extends WorkflowDef<Reg, any, any, any, any>>(
-      workflow: W,
-      input: W extends WorkflowDef<Reg, infer I, any, any, any> ? I : never,
-      observers?: WorkflowObserver[],
-    ): Promise<{
-      output: W extends WorkflowDef<Reg, any, any, any, infer O> ? O : never;
-      results: W extends WorkflowDef<Reg, any, infer R, any, any> ? R : never;
-      extras: Record<string, any>;
-    }> => {
-      return executeWorkflow(workflow, registry, input, context, observers);
-    },
-  };
-}
+// export function createRuntime<Reg extends ActionRegistry, Context>(
+//   registry: Reg,
+//   context: Context,
+// ) {
+//   return {
+//     run: async <W extends WorkflowDef<Reg, any, any, any, any>>(
+//       workflow: W,
+//       input: W extends WorkflowDef<Reg, infer I, any, any, any> ? I : never,
+//       observers?: WorkflowObserver[],
+//     ): Promise<{
+//       output: W extends WorkflowDef<Reg, any, any, any, infer O> ? O : never;
+//       results: W extends WorkflowDef<Reg, any, infer R, any, any> ? R : never;
+//       extras: Record<string, any>;
+//     }> => {
+//       return executeWorkflow(workflow, registry, input, context, observers);
+//     },
+//   };
+// }
